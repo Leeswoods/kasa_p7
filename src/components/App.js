fb640936b9957209a95ea66f419669048687c37e
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-11-24 15:12:21 +0100

    Ajout de la  structure du code

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+// Importe le module banner.js
+import Banner from './Banner'
+
+// Affiche le code 
+function App() {
+    return <Banner />
+}
+
+export default App
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import '../styles/Banner.css'
+
+function Banner() {
+    // return (
+    //     <div className='lmj-banner'>
+    //         <h1>La maison jungle</h1>
+    //     </div>
+    // )
+}
+
+// export default : Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import. 
+export default Banner
