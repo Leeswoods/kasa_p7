6e40a0a19cc738363f80df804127fe08f7fcdbb1
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-11-28 18:57:43 +0100

    Ajout fichiers

--- src/components/Banner.js
+++
@@ -1,6 +0,0 @@
-import "../styles/Banner.css";
-
-function Banner() {}
-
-// export default : Il s'agit d'une syntaxe prévue dans l'ES6, qui vous épargnera d'utiliser les accolades au moment de l'import.
-export default Banner;
