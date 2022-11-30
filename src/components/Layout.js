a97d6fcc5973bb8d14ccc556b8353f15438d68ae
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-11-30 17:47:15 +0100

    modif

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import "../styles/components/Layout.scss";
 
 //  Mise en place du Layout, constitué des composants "Header" et "Footer" et appelé une fois pour toutes dans le composant général index.js
 const Layout = (props) => {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className="body">
+      <div className="Layout">
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   );
