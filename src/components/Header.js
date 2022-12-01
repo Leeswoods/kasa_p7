76f0abf545ef5dde8198ba5418218c5aa38f16e7
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-12-01 20:30:55 +0100

    About Page + Banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import "../styles/components/Header.scss";
 import { NavLink } from "react-router-dom"; //  <NavLink> est un type particulier de <Link> qui sait s'il est "actif" ou non. Ceci est utile lors de la création d'un menu de navigation
 import Logo from "../assets/LOGO.png";
 
+// Navlink : isActive permet de savoir si l'utilisateur est sur quelle page
+// isActive : fonction : Une fonction pour ajouter une logique supplémentaire pour déterminer si le lien est actif.
+
 // Composant "Header", utilisation de NavLink pour la redirection des liens "Accueil" et "À propos"
 const Header = () => {
   return (
@@ -36,16 +39,3 @@ const Header = () => {
 };
 
 export default Header;
-
-//         <li>
-//   {/* To sert à indiquer la route de la navigation */}
-//   <NavLink to="/" className="homelink">
-//     Accueil
-//   </NavLink>
-// </li>
-// <li>
-//   {/* To sert à indiquer la route de la navigation */}
-//   <NavLink to="/about" className="aboutlink">
-//     A propos
-//   </NavLink>
-// </li>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import Banner from "../components/Banner";
+import BGBannerAbout from "../assets/a_propos_bg_bannière.png";
+import "../styles/pages/About.scss";
+// import DataAbout from "../data/about.json";
 
+const About = () => {
+  document.title = "Kasa - A propos de nous";
+
+  return (
+    <main className="display_about">
+      <Banner img={BGBannerAbout} />
+    </main>
+  );
+};
+
+export default About;
