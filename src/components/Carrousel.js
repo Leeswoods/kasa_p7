c39f5ee0345ed2fe266af52d22141c25cd4226bf
Damien Ouvrard <ouvrard.damien33@gmail.com>
2022-12-06 02:35:24 +0100

    Composant Carrousel & Rate (étoile)

---
+++ src/components/Carrousel.js
@@ -0,0 +1,68 @@
+// Importation Package, icon, style, etc...
+import React, { useState } from "react";
+
+import "../styles/components/Carrousel.scss";
+
+// Icon flèche
+const ArrowRight = <i className="fa-solid fa-chevron-right"></i>;
+const ArrowLeft = <i className="fa-solid fa-chevron-left"></i>;
+
+const Carrousel = ({ pictures }) => {
+  // Le hook useState permet de faire varier l'index de l'array pictures, état initial à 0
+  const [current, setCurrent] = useState(0); // première image à 0
+
+  const length = pictures.length; // Longueur du tableau pictures
+
+  // Fonction previousPicture qui actualise l'index au clic sur la flèche "précèdent" du carrousel
+  const previousPicture = () => {
+    // L'opérateur (ternaire) conditionnel => condition ? exprSiVrai : exprSiFaux
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  // Fonction nextPicture qui actualise l'index de l'array pictures au clic sur la flèche "suivante" du carousel
+  const nextPicture = () => {
+    // L'opérateur (ternaire) conditionnel => condition ? exprSiVrai : exprSiFaux
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  return (
+    <div className="slider">
+      <img
+        className="slideImg"
+        // L'index de l'array, matérialisé par 'currentIndex', est rendu dynamique grâce au hook useState
+        src={pictures[current]}
+        alt={pictures.title}
+        key={current}
+      />
+
+      {/* Affichage des flèches seulement si length > 1 */}
+      {length > 1 && (
+        // Affichage conditionnel de la flèche "précédent" en fonction du nombre d'images dans l'array pictures
+        <img
+          src={ArrowLeft} //Affichage des flèches seulement si length > 1
+          // Appel la fonction previousPicture au clique sur la flèche
+          onClick={previousPicture}
+          alt="flèche gauche"
+          className="leftArrow"
+        />
+      )}
+
+      {/* Conteur */}
+      <div className="slideCounter">
+        {current + 1}/{length}
+      </div>
+
+      {length > 1 && (
+        <img
+          src={ArrowRight}
+          alt="flèche droite"
+          // Appel la fonction nextPicture au clique sur la flèche
+          onClick={nextPicture}
+          className="rightArrow"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Carrousel;
---
+++ src/components/Rate.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../styles/utils/_variables.scss";
+import "../styles/components/Rate.scss";
+
+// Mise en place du composantt Rating qui va permettre de savoir le taux d'étoile dans une page logement
+const Rate = ({ rating }) => {
+  // Tableau d'étoiles
+  const stars = [1, 2, 3, 4, 5];
+
+  // Icon Etoile rouge
+  const star = (
+    <i style={{ color: "$color-1" }} className="fa-solid fa-star"></i>
+  );
+  // Icon Etoile Grise
+  const greyStar = (
+    <i style={{ color: "#E3E3E3" }} className="fa-solid fa-star"></i>
+  );
+
+  return (
+    // La méthode map() crée un nouveau tableau avec les résultats de l'appel d'une fonction fournie sur chaque élément du tableau appelant.
+    stars.map((level, index) =>
+      // Si le nombre correspondant à rangeElem est plus petit ou égal au chiffre contenu dans la prop rating,
+      // on affiche le nombre d'étoiles rouge équivalent
+      level <= rating ? (
+        <img src={star} alt="étoile pleine" key={index} className="starIcon" />
+      ) : (
+        // La différence entre range et rating est affichée par des étoiles grises
+        <img
+          src={greyStar}
+          alt="étoile vide"
+          key={index}
+          className="starIcon"
+        />
+      )
+    )
+  );
+};
+
+export default Rate;
